c25069ed2545e1ae1f7ee5b43a40bb883aa6c0d6
Cem Safa <c0820726@mylambton.ca>
2021-11-09 13:52:42 -0500

    initial commit

---
+++ Cravings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cravings
+//
+//  Created by Cem Safa on 2021-11-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
