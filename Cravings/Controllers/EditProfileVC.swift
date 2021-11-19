ce3ea8392a98eb97dc743f743b235645f09d8d95
Kristina Ginga <c0807287@mylambton.ca>
2021-11-19 17:38:04 -0500

    added IBAction and IBOutlet on Edit Profile VC

--- Cravings/Controllers/EditProfileVC.swift
+++ Cravings/Controllers/EditProfileVC.swift
@@ -10,8 +10,14 @@ import UIKit
 class EditProfileVC: UIViewController {
     
     @IBOutlet weak var editProfileImage: UIImageView!
+    @IBOutlet weak var addPhotoLbl: UILabel!
     
-
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var webYTField: UITextField!
+    @IBOutlet weak var aboutMeField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,5 +27,16 @@ class EditProfileVC: UIViewController {
        
     }
     
-
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func addPhotoLblTapped (sender: UITapGestureRecognizer) {
+        
+    }
+    
+    
+    
+    
+    
+    
 }
