497e7722013b55169b8152f777800eef59f8ba20
Kristina Ginga <c0807287@mylambton.ca>
2021-11-19 21:40:11 -0500

    added functionality to add photo button. Editor Profile VC done.

--- Cravings/Controllers/EditProfileVC.swift
+++ Cravings/Controllers/EditProfileVC.swift
@@ -18,14 +18,20 @@ class EditProfileVC: UIViewController {
     @IBOutlet weak var webYTField: UITextField!
     @IBOutlet weak var aboutMeField: UITextField!
     
+    let picker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-      
+          
         editProfileImage.roundedImage()
-
-       
+        
+        let addPhotoTappedRecognizer = UITapGestureRecognizer(target: self, action: #selector(addPhotoLblTapped))
+        addPhotoLbl.isUserInteractionEnabled = true
+        addPhotoLbl.addGestureRecognizer(addPhotoTappedRecognizer)
+        
     }
+    
+    
     
     @IBAction func saveButtonTapped(_ sender: UIButton) {
     }
@@ -33,13 +39,34 @@ class EditProfileVC: UIViewController {
     @IBAction func backButton(_ sender: UIButton) {
     }
     
+   
     @IBAction func addPhotoLblTapped (sender: UITapGestureRecognizer) {
         
+        picker.allowsEditing = true
+        //picker.sourceType = .camera   //camera will work on real ios device
+        picker.delegate = self
+        present(picker, animated: true)
+        
+               
     }
     
+   
+}
+
+extension EditProfileVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
     
-    
-    
-    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+     
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+            return
+        }
+        editProfileImage.image = image
+        
+    }
 }
