91e25b3cd359e976a6fada2c26871bac6572264b
Kristina Ginga <c0807287@mylambton.ca>
2021-11-21 21:28:34 -0500

    added minimum line & inter-item spacing to collection view

--- Cravings/Controllers/HomeVC.swift
+++ Cravings/Controllers/HomeVC.swift
@@ -9,11 +9,22 @@ import UIKit
 import FirebaseAuth
 
 class HomeVC: UIViewController {
+    
+    var collectionView: UICollectionView?
+    var screenSize: CGRect!
+    var screenWidth: CGFloat!
+    var screenHeight: CGFloat!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        let width = UIScreen.main.bounds.width
+        layout.sectionInset = UIEdgeInsets(top: 2, left: 0, bottom: 0, right: 0)
+        layout.itemSize = CGSize(width: width / 2, height: width / 2)
+        layout.minimumInteritemSpacing = 5
+        layout.minimumLineSpacing = 5
+        collectionView?.collectionViewLayout = layout
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -28,4 +39,30 @@ class HomeVC: UIViewController {
             present(loginVC, animated: false)
         }
     }
+}
+
+extension HomeVC : UICollectionViewDelegateFlowLayout{
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return UIEdgeInsets(top: 2, left: 0, bottom: 0, right: 0)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let collectionViewWidth = collectionView.bounds.width
+        return CGSize(width: collectionViewWidth/3, height: collectionViewWidth/3)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    
+    
+   
+    
+    
 }
--- Cravings/Controllers/ProfileVC.swift
+++ Cravings/Controllers/ProfileVC.swift
@@ -8,17 +8,29 @@
 import UIKit
 
 class ProfileVC: UIViewController {
-
+    
+    
     @IBOutlet weak var profileImage: UIImageView!
     
     @IBOutlet weak var nameLbl: UILabel!
     @IBOutlet weak var titleLbl: UILabel!
     @IBOutlet weak var followersCountLbl: UILabel!
     
+        var collectionView: UICollectionView?
+        var screenSize: CGRect!
+        var screenWidth: CGFloat!
+        var screenHeight: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+            let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+            let width = UIScreen.main.bounds.width
+            layout.sectionInset = UIEdgeInsets(top: 2, left: 0, bottom: 0, right: 0)
+            layout.itemSize = CGSize(width: width / 2, height: width / 2)
+            layout.minimumInteritemSpacing = 5
+            layout.minimumLineSpacing = 5
+            collectionView?.collectionViewLayout = layout
     }
     
     
@@ -31,7 +43,33 @@ class ProfileVC: UIViewController {
     @IBAction func followBtnPressed(_ sender: UIButton) {
     }
     
-    
-    
 
+    
+  
+}
+
+extension ProfileVC : UICollectionViewDelegateFlowLayout{
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return UIEdgeInsets(top: 2, left: 0, bottom: 0, right: 0)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let collectionViewWidth = collectionView.bounds.width
+        return CGSize(width: collectionViewWidth/3, height: collectionViewWidth/3)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    
+    
+   
+    
+    
 }
