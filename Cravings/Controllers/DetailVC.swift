a77960332c48c51cf14aab8fb6baa0be7e938516
Kristina Ginga <c0807287@mylambton.ca>
2021-11-23 15:46:11 -0500

    started with Detail VC, added image views for photo or video, labels, heart and back buttons

---
+++ Cravings/Controllers/DetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  DetailVC.swift
+//  Cravings
+//
+//  Created by Ma. Kristina Ginga on 2021-11-23.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
