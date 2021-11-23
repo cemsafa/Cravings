55d413fec882695f5f1737e35781a9fd0586992c
Kristina Ginga <c0807287@mylambton.ca>
2021-11-23 18:45:04 -0500

    added comment button, IB Outlet & IB Action

--- Cravings/Controllers/DetailVC.swift
+++ Cravings/Controllers/DetailVC.swift
@@ -6,15 +6,34 @@
 //
 
 import UIKit
+import SwiftUI
 
 class DetailVC: UIViewController {
-
+    
+    
+    @IBOutlet weak var detailImage: UIImageView!
+    @IBOutlet weak var userImage: UIImageView!
+    
+    @IBOutlet weak var fullNameLbl: UILabel!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var likeCountLbl: UILabel!
+    @IBOutlet weak var captionLbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+      
     }
     
-
-
+    
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func likeBtnPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func commentBtnPressed(_ sender: UIButton) {
+    }
 }
+
