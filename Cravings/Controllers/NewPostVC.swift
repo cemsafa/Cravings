afdc9fb0cc40e5c38ff00a4bf09c9eeafbb68aa3
Kristina Ginga <c0807287@mylambton.ca>
2021-11-28 16:51:25 -0500

    replaced label with textfield

---
+++ Cravings/Controllers/NewPostVC.swift
@@ -0,0 +1,18 @@
+//
+//  NewPost1VC.swift
+//  Cravings
+//
+//  Created by Ma. Kristina Ginga on 2021-11-28.
+//
+
+import UIKit
+
+class NewPostVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
